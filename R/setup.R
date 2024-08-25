d25d9437a9305a7835faba1860a3339dd9adfe62
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-08-25 15:14:42 +0200

    Initial commit

---
+++ R/setup.R
@@ -0,0 +1,33 @@
+classes <- c(
+  "Introduktion til kurset og R",
+  "Datahåndtering og manipulation i R",
+  "Geo-data",
+  "Geo-data visualisering og kortlægning",
+  "Geo-dataanalyse I - Geokodning, afstande, mønstre og klynger",
+  "Geo-dataanalyse II - Måling, Måling, Måling",
+  "Geo-dataanalyse III - Statistisk læring og web scraping",
+  "Naturlige eksperimenter og geografiske RDD I",
+  "Naturlige eksperimenter og geografiske RDD II",
+  "Projekt-feedback",
+  "Analyse: Ulighed, kriminalitet, race",
+  "Analyse: Klima",
+  "Projekt-præsentationer",
+  "Projekt-præsentationer"
+)
+
+aleksandeR::prepare_classes(c(
+  "Introduktion til kurset og R",
+  "Datahåndtering og manipulation i R",
+  "Geo-data",
+  "Geo-data visualisering og kortlægning",
+  "Geo-dataanalyse I - Geokodning, afstande, mønstre og klynger",
+  "Geo-dataanalyse II - Måling, Måling, Måling",
+  "Geo-dataanalyse III - Statistisk læring og web scraping",
+  "Naturlige eksperimenter og geografiske RDD I",
+  "Naturlige eksperimenter og geografiske RDD II",
+  "Projekt-feedback",
+  "Analyse: Ulighed, kriminalitet, race",
+  "Analyse: Klima",
+  "Projekt-præsentationer",
+  "Projekt-præsentationer"
+))
