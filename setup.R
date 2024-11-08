1eb068c4257945f73bb2461ec269a467934738f0
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-11-08 22:46:05 +0100

    Add getting started example from keras3

--- setup.R
+++ setup.R
@@ -2,7 +2,7 @@
 
 config <- reticulate::py_config()
 if (config$pythonhome |> basename() == "r-keras") {
-  print("Env setup")
+  print("Env er setup")
 } else {
   keras3::install_keras(
     backend = "tensorflow"
@@ -24,3 +24,44 @@ x_test <- array_reshape(x_test, c(nrow(x_test), 784))
 # rescale
 x_train <- x_train / 255
 x_test <- x_test / 255
+
+y_train <- to_categorical(y_train, 10)
+y_test <- to_categorical(y_test, 10)
+
+model <- keras_model_sequential(input_shape = c(784))
+
+model |>
+  layer_dense(units = 256, activation = "relu") |>
+  layer_dropout(rate = 0.4) |>
+  layer_dense(units = 128, activation = "relu") |>
+  layer_dropout(rate = 0.3) |>
+  layer_dense(units = 10, activation = "softmax")
+
+summary(model)
+plot(model)
+
+model |>
+  compile(
+    loss = "categorical_crossentropy",
+    optimizer = optimizer_rmsprop(),
+    metrics = c("accuracy")
+  )
+
+history <- model |> fit(
+  x_train, y_train,
+  epochs = 30, batch_size = 128,
+  validation_split = 0.2
+)
+
+plot(history)
+
+model |>
+  evaluate(x_test, y_test)
+
+probs <- model |>
+  predict(x_test)
+max.col(probs) - 1L
+
+
+
+
