f30e488cd41798109096346ae6e7455c34f47576
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-11-22 11:21:46 +0100

    Get data

--- _targets.R
+++ _targets.R
@@ -30,6 +30,20 @@ tar_source()
 
 # Replace the target list below with your own:
 list(
+  # Importer træningsdata
+  tar_target(t_b02, "/Volumes/T7 Shield/Remote sensing/VESTJ/B02.jp2", format = "file"),
+  tar_target(t_b03, "/Volumes/T7 Shield/Remote sensing/VESTJ/B03.jp2", format = "file"),
+  tar_target(t_b04, "/Volumes/T7 Shield/Remote sensing/VESTJ/B04.jp2", format = "file"),
+  tar_target(t_b08, "/Volumes/T7 Shield/Remote sensing/VESTJ/B08.jp2", format = "file"),
+  # Importer valideringsdata
+  tar_target(v_b02, "/Volumes/T7 Shield/Remote sensing/NRVST/B02.jp2", format = "file"),
+  tar_target(v_b03, "/Volumes/T7 Shield/Remote sensing/NRVST/B03.jp2", format = "file"),
+  tar_target(v_b04, "/Volumes/T7 Shield/Remote sensing/NRVST/B04.jp2", format = "file"),
+  tar_target(v_b08, "/Volumes/T7 Shield/Remote sensing/NRVST/B08.jp2", format = "file"),
+  # Sammensæt træning raster
+  tar_target(t_rast, import_images(t_b02, t_b03, t_b04, t_b08)),
+  # Sammensæt validation raster
+  tar_target(v_rast, import_images(v_b02, v_b03, v_b04, v_b08)),
   tar_target(point, st_point(c(0, 0))),
   tar_target(cat_1_m, 4),
   tar_target(cat_2_m, cat_1_m * 2),
