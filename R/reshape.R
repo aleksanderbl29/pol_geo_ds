e0c997383e385d904ba7fb57ff219a41c176867a
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-12-20 21:04:29 +0100

    Converter til kombi af terra og stars

--- R/reshape.R
+++ R/reshape.R
@@ -1,23 +1,20 @@
-reshape <- function(input) {
-  # Normaliser raster værdier
-  rast <- unwrap(input)
-  rast <- rast / max(rast)
+reshape <- function(b02, b03, b04, b08, stars) {
 
+  # Importerer rasters
+  b2_rast <- rast(b02)
+  b3_rast <- rast(b03)
+  b4_rast <- rast(b04)
+  b8_rast <- rast(b08)
 
-  ## TO DO
-  # TIlføj nyt lag hvis et lag hedder noget med vindmølle
-  # add(new_layer) <- new_layer
+  # Stack rasters
+  raster <- c(b2_rast, b3_rast, b4_rast, b8_rast)
 
-
-  if (names(rast)[1] == "Vindmølle") {
-    # Tilføj et lag mere
-    add(rast) <- rast
-    # Læg de to lag oveni to mere
-    add(rast) <- rast
-  }
+  # Crop rasteren så den passer med star
+  raster <- crop(raster, st_bbox(stars))
 
   # Konverter til array
-  data <- as.array(rast)
+  data <- raster |>
+    as.array()
 
   # Reshape så Keras forstår formatet
   array_reshape(data, c(dim(data)[1], dim(data)[2], dim(data)[3], 1))
